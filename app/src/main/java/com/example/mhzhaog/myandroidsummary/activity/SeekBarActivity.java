cbe71e22f75d9230cd3e50132118fef84c619e5f
BSSYNHDJZMH <569334386@qq.com>
2019-03-19 23:49:25 +0800

    弹出对话框

--- app/src/main/java/com/example/mhzhaog/myandroidsummary/activity/SeekBarActivity.java
+++ app/src/main/java/com/example/mhzhaog/myandroidsummary/activity/SeekBarActivity.java
@@ -1,12 +1,17 @@
 package com.example.mhzhaog.myandroidsummary.activity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.Toast;
 import com.example.mhzhaog.myandroidsummary.R;
 
 import java.util.Timer;
@@ -21,6 +26,7 @@ public class SeekBarActivity extends AppCompatActivity {
     private Button buttonPlay;
     private Button buttonPause;
     private ProgressBar progressBar;
+    private Button buttonDownload;
 
 
     @Override
@@ -36,6 +42,7 @@ public class SeekBarActivity extends AppCompatActivity {
         buttonPlay = findViewById(R.id.buttonPlay);
         buttonPause = findViewById(R.id.buttonPause);
         progressBar = findViewById(R.id.progressBar);
+        buttonDownload = findViewById(R.id.buttonDownload);
 
         buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,6 +89,53 @@ public class SeekBarActivity extends AppCompatActivity {
 
             }
         });
+
+        buttonDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialg();
+            }
+        });
+    }
+
+    private void showDialg() {
+        //    通过AlertDialog.Builder这个类来实例化我们的一个AlertDialog的对象
+        AlertDialog.Builder builder = new AlertDialog.Builder(SeekBarActivity.this);
+        //    设置Title的图标
+        builder.setIcon(R.mipmap.ic_launcher);
+        //    设置Title的内容
+        builder.setTitle("弹出警告框");
+        //    设置Content来显示一个信息
+        builder.setMessage("确定删除吗？");
+        //    设置一个PositiveButton
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Toast.makeText(SeekBarActivity.this, "positive: " + which, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //    设置一个NegativeButton
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Toast.makeText(SeekBarActivity.this, "negative: " + which, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //    设置一个NeutralButton
+        builder.setNeutralButton("忽略", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Toast.makeText(SeekBarActivity.this, "neutral: " + which, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //    显示出该对话框
+        builder.show();
     }
 
     private void countTime() {
